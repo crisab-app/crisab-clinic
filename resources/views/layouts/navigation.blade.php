53071a9d05f274b9bb17c881f88e3ecb4061dd54
ETorres76 <administracion@crisab.com>
2026-04-04 21:23:06 -0500

    Agregar boton de clinicas al menu del superadmin

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,13 +10,17 @@
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <!-- Menu para superadmin -->                    
+                    @role('Superadmin')
+                        <x-nav-link :href="route('clinics.index')" :active="request()->routeIs('clinics.*')">
+                            {{ __('Clínicas') }}
+                        </x-nav-link>
+                    @endrole
                 </div>
-            </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
@@ -67,9 +71,16 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
+
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <!-- Menu para superadmin -->
+            @role('Superadmin')
+                <x-responsive-nav-link :href="route('clinics.index')" :active="request()->routeIs('clinics.*')">
+                    {{ __('Clínicas') }}
+                </x-responsive-nav-link>
+            @endrole
         </div>
 
         <!-- Responsive Settings Options -->
