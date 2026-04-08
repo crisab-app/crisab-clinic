a862a557c70501f03a626c33096c8e9503a085c6
ETorres76 <administracion@crisab.com>
2026-04-08 15:48:39 -0500

    idioma verificar correo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Login;
@@ -31,6 +33,15 @@ public function boot(): void
                 'ip_address' => request()->ip(),
                 'user_agent' => request()->userAgent(),
             ]);
+            VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->subject('Verifica tu correo electrónico - Adminconsul')
+                ->greeting('¡Hola!')
+                ->line('Por favor, haz clic en el botón de abajo para verificar tu dirección de correo electrónico y acceder a tu panel.')
+                ->action('Verificar Correo', $url)
+                ->line('Si no creaste esta cuenta, no es necesario realizar ninguna acción.')
+                ->salutation('Saludos, el equipo de Adminconsul');
+        });
         });
     }
 }
