60fe5cb7bf0b1a733571d546f124cd6f1e0b7084
ETorres76 <administracion@crisab.com>
2026-04-08 22:21:59 -0500

    fix navigation blade

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,26 +1,26 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 dark:bg-gray-800 dark:border-gray-700">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
-            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    
+
                     @role('Administrador de Clinica')
                     <x-nav-link :href="route('clinic-resources.index')" :active="request()->routeIs('clinic-resources.*')">
                         {{ __('Recursos') }}
                     </x-nav-link>
                     @endrole
                 </div>
-                </div>
-            </div> 
+            </div>
+
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 
                 <button x-data="{ isDark: document.documentElement.classList.contains('dark') }" 
@@ -46,13 +46,8 @@
                 </button>
 
                 <x-dropdown align="right" width="48">
-
-
-
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white dark:bg-gray-800 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ms-1">
@@ -94,20 +89,26 @@
 
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            
+            @role('Administrador de Clinica')
+            <x-responsive-nav-link :href="route('clinic-resources.index')" :active="request()->routeIs('clinic-resources.*')">
+                {{ __('Recursos') }}
+            </x-responsive-nav-link>
+            @endrole
+
             @role('Superadmin')
-                <x-responsive-nav-link :href="route('clinics.index')" :active="request()->routeIs('clinics.*')">
-                    {{ __('Clínicas') }}
-                </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('clinics.index')" :active="request()->routeIs('clinics.*')">
+                {{ __('Clínicas') }}
+            </x-responsive-nav-link>
             @endrole
         </div>
 
-        <div class="pt-4 pb-1 border-t border-gray-200">
+        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
 
