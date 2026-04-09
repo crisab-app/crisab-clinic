050498dcc44219c7706b656f17aa02182fef7ab6
ETorres76 <administracion@crisab.com>
2026-04-08 22:46:29 -0500

    fix navigation blade 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,8 +24,8 @@
     </head>
     </head>
 
-  <body class="font-sans antialiased bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
-        <div class="min-h-screen bg-gray-100">
+<body class="font-sans antialiased bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+            <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
