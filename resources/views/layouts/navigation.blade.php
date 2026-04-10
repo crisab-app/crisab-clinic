f10861ae4fe35bf6eb4808f8e9abf2d6cff86b62
ETorres76 <administracion@crisab.com>
2026-04-10 12:38:39 -0500

    menus y catalogos staff y recursos

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -14,12 +14,32 @@
                     </x-nav-link>
 
                     @role('Administrador de Clinica')
-                    <x-nav-link :href="route('clinic-resources.index')" :active="request()->routeIs('clinic-resources.*')">
-                        {{ __('Recursos') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('staff.index')" :active="request()->routeIs('staff.*')">
-                        {{ __('Personal') }}
-                    </x-nav-link>
+                    <div class="hidden sm:flex sm:items-center sm:ms-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                    <div>Configuración</div>
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('staff.index')">
+                                    {{ __('Personal') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('clinic-resources.index')">
+                                    {{ __('Recursos Físicos') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('catalogs.index')">
+                                    {{ __('Especialidades y Catálogos') }}
+                                </x-dropdown-link>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
                     @endrole
 
                     @role('Superadmin')
@@ -103,12 +123,25 @@
             </x-responsive-nav-link>
             
             @role('Administrador de Clinica')
-            <x-responsive-nav-link :href="route('clinic-resources.index')" :active="request()->routeIs('clinic-resources.*')">
-                {{ __('Recursos') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('staff.index')" :active="request()->routeIs('staff.*')">
-                {{ __('Personal') }}
-            </x-responsive-nav-link>
+            <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+                <div class="px-4 mb-2">
+                    <div class="font-medium text-xs text-gray-500 dark:text-gray-400 uppercase tracking-widest">Configuración</div>
+                </div>
+
+                <div class="mt-2 space-y-1">
+                    <x-responsive-nav-link :href="route('staff.index')" :active="request()->routeIs('staff.*')">
+                        {{ __('Personal') }}
+                    </x-responsive-nav-link>
+                    
+                    <x-responsive-nav-link :href="route('clinic-resources.index')" :active="request()->routeIs('clinic-resources.*')">
+                        {{ __('Recursos Físicos') }}
+                    </x-responsive-nav-link>
+                    
+                    <x-responsive-nav-link :href="route('catalogs.index')" :active="request()->routeIs('catalogs.*')">
+                        {{ __('Especialidades y Catálogos') }}
+                    </x-responsive-nav-link>
+                </div>
+            </div>
             @endrole
 
             @role('Superadmin')
