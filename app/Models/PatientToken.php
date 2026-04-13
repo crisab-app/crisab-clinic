349ac0964a4a61c7403dbfdeccde7ee2482d84c7
ETorres76 <administracion@crisab.com>
2026-04-12 23:56:58 -0500

    alta de pacientes token

--- app/Models/PatientToken.php
+++ app/Models/PatientToken.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PatientToken extends Model
 {
-    //
+    use HasFactory;
+
+    // 1. Damos permiso para guardar estos campos masivamente
+    protected $fillable = [
+        'patient_id',
+        'token',
+        'expires_at',
+        'is_used'
+    ];
+
+    // 2. Le decimos a Laravel que este token le pertenece a un Paciente
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
 }
