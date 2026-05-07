6ed508ff69e80a1f456edf6763268bbdd5b779cd
ETorres76 <administracion@crisab.com>
2026-05-07 14:44:32 -0500

    Fix: Validar existencia de tabla activity_log

--- database/migrations/2026_05_06_192729_add_event_column_to_activity_log_table.php
+++ database/migrations/2026_05_06_192729_add_event_column_to_activity_log_table.php
@@ -6,10 +6,13 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddEventColumnToActivityLogTable extends Migration
 {
-    public function up()
+public function up()
     {
-        Schema::connection(config('activitylog.database_connection'))->table(config('activitylog.table_name'), function (Blueprint $table) {
-            $table->string('event')->nullable()->after('subject_type');
+        Schema::table('activity_log', function (Blueprint $table) {
+            // Verificamos si la columna 'event' NO existe antes de crearla
+            if (!Schema::hasColumn('activity_log', 'event')) {
+                $table->string('event')->nullable()->after('subject_type');
+            }
         });
     }
 
